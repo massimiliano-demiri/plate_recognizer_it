4942062bf53bc44b4073bd38f5ab9a8cd0e133a3
Massimiliano Demiri <150348788+massimiliano-demiri@users.noreply.github.com>
2025-04-30 09:17:39 +0200

    fix

--- server.py
+++ server.py
@@ -61,7 +61,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["POST", "GET"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
